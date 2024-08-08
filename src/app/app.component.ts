26f5c95bcfaa9d43e3730d64ddcf13a4c91fb88b
Ner Aguilar <ner.aguilar06@gmail.com>
2024-08-08 18:44:03 -0400

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'roofing-corp';
+  vaCurrentSlide: number = 0;
+  images: any | []  = [
+    {   
+      counter: 1,
+      img: '../assets/roofWork1.jpg'
+    },
+    {   
+      counter: 2,
+      img: '../assets/roofWork2.jpg'
+    },
+  ]
+
+  ngOnInit(): void {
+    this.onChangeImage();
+  }
+
+  onChangeImage() {
+    setInterval(() => {
+      if (this.vaCurrentSlide < this.images.length - 1) {
+        console.log(this.vaCurrentSlide);
+        this.vaCurrentSlide++;
+      }
+      else {
+        this.vaCurrentSlide = 0
+      }
+    }, 3000)
+  }
+
+  onPreviousClick() {
+    const previous = this.vaCurrentSlide - 1;
+    this.vaCurrentSlide = previous < 0 ? this.images.length - 1 : previous
+  }
+
+  onNextClick() {
+    const next = this.vaCurrentSlide + 1;
+    this.vaCurrentSlide = next === this.images.length ? 0 : next;
+  }
+
+}
